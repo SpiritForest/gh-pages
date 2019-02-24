e05691a7c6906fef2a54cf07ba0fd7f4c05f3017
spiritforest <dzahar@mamil.ru>
2019-02-24 10:14:24 +0300

    Updates

---
+++ precache-manifest.c76499591a70da38d78530ec8c771be3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5357f7305c488abea30c",
+    "url": "/mentor-dashboard/static/js/runtime~main.5357f730.js"
+  },
+  {
+    "revision": "8b922ecbcb2769b80415",
+    "url": "/mentor-dashboard/static/js/main.8b922ecb.chunk.js"
+  },
+  {
+    "revision": "bf64de9bf10e6b4f44b8",
+    "url": "/mentor-dashboard/static/js/2.bf64de9b.chunk.js"
+  },
+  {
+    "revision": "8b922ecbcb2769b80415",
+    "url": "/mentor-dashboard/static/css/main.097f5c15.chunk.css"
+  },
+  {
+    "revision": "bbecb4f933175f4e3241bf3ac741a940",
+    "url": "/mentor-dashboard/index.html"
+  }
+];
